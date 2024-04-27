b840476995a796d5ae875fa0265f706278ce294e
0dot77 <yty0706@gmail.com>
2024-04-27 10:48:49 +0900

    0427 2차원 배열, 배열과 포인터, std::vector

---
+++ cpp-200-085/cpp-200-085.cpp
@@ -0,0 +1,49 @@
+// cpp-200-085.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void Func1(int arr[2][2])
+{
+    arr[0][0] = 1000;
+}
+
+void Func2(int arr[][2], int row)
+{
+    arr[row - 2][1] = 2000;
+}
+
+void Func3(int* arr, int row, int col)
+{
+    *((arr + row - 1) + col - 1) = 3000;
+}
+
+void Func4(int arr)
+{
+    
+}
+
+int main()
+{
+    int data[2][2]{ {1,2}, {3,4} };
+
+    Func1(data);
+    Func2(data, 2);
+    Func3(*data, 2, 2);
+
+    cout << "== °á°ú ==" << endl;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << data[i][j] << endl;
+        }
+    }
+
+
+    return 0;
+}
+
